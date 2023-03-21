81de00361730b0c201a8f9006471d2defb8ba4b0
isky27 <gaganjindal50@gmail.com>
2023-03-21 10:26:39 +0530

    update

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -5,24 +5,7 @@ const User = require("../models/userModel");
 
 const accessChat = asyncHandler(async (req, res) => {
     const { userId } = req.body;
-    if (!userId) {
-        console.log("UserId param not sent with request");
-        return res.sendStatus(400);
-    }
-    var isChat = await Chat.find({
-        isGroupChat: false,
-        $and: [
-            { users: { $elemMatch: { $eq: req.user._id } } },
-            { users: { $elemMatch: { $eq: userId } } },
-        ],
-    })
-        .populate("users", "-password")
-        .populate("latestMessage");
-
-    isChat = await User.populate(isChat, {
-        path: "latestMessage.sender",
-        select: "name pic email",
-    });
+   
 
     if (isChat.length > 0) {
         res.send(isChat[0]);
