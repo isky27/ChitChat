143e53ef52f9dc16d8cc7911a32e2e5f1e8b24e2
isky27 <gaganjindal50@gmail.com>
2023-04-08 13:40:50 +0530

    function update

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -94,6 +94,28 @@ const createGroupChat = asyncHandler(async (req, res) => {
     }
 });
 
+const renameGroup = asyncHandler(async (req, res) => {
+    const { chatId, chatName } = req.body;
+
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            chatName: chatName,
+        },
+        {
+            new: true,
+        }
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+
+    if (!updatedChat) {
+        res.status(404);
+        throw new Error("Chat Not Found");
+    } else {
+        res.json(updatedChat);
+    }
+});
 
 const addToGroup = asyncHandler(async (req, res) => {
     const { chatId, userId } = req.body;
